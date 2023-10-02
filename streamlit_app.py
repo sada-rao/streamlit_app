c644bbc1fcb78258a5a838083166d4c9e8b207b1
Sadanand Rao <30704300+sada-rao@users.noreply.github.com>
2023-10-02 14:48:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,15 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+#Create a function
+def get_fruityvice_data(fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  # Normalize JSON in a pandas dataframe 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -24,11 +33,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    # Normalize JSON in a pandas dataframe 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return_func = get_fruityvice_data(fruit_choice)
     # display the json in dataframe format
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(return_func)
 except URLError as e:
   streamlit.error()
 
